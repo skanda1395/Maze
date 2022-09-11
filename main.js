3bda1a1ce2b3ce00bfc8e7c125ec717b1b0b16ae
Sunil <skanda1395@gmail.com>
2022-09-11 13:39:39 +0530

    move ball

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const alpha = document.getElementById("alpha");
 const beta = document.getElementById("beta");
 const gamma = document.getElementById("gamma");
+const ball = document.getElementById("ball");
+
 
 window.addEventListener('deviceorientation', (event) => {
     alpha.innerText = Math.floor(event.alpha);
     beta.innerText = Math.floor(event.beta);
     gamma.innerText = Math.floor(event.gamma);
+
+    // move ball
+    ball.position.top = `${beta}px`;
+    ball.position.left = `${gamma}px`
 });
   
