54ab23b94ef0eceadc4faa8e401d3b24721e80f9
Sunil <skanda1395@gmail.com>
2022-09-11 13:08:09 +0530

    round values

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const beta = document.getElementById("beta");
 const gamma = document.getElementById("gamma");
 
 window.addEventListener('deviceorientation', (event) => {
-    alpha.innerText = event.alpha;
-    beta.innerText = event.beta;
-    gamma.innerText = event.gamma;
+    alpha.innerText = Math.floor(event.alpha);
+    beta.innerText = Math.floor(event.beta);
+    gamma.innerText = Math.floor(event.gamma);
 });
   
